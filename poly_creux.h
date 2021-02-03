631c45f6a1d05fda0350e80fe45294d411a21174
Thomas Abecassis <stingerkiller34@gmail.com>
2021-02-03 16:16:20 +0100

    poly creux h

---
+++ poly_creux.h
@@ -0,0 +1,34 @@
+typedef struct {
+    int nb_degree;
+    p_element* elements;
+} polyf_creux_t, *p_polyf_creux_t;
+
+typedef struct{
+    int degree;
+    int coeff;
+}element, *p_element;
+
+
+p_polyf_t creer_polynome (int degre) ;
+
+void init_polynome (p_polyf_t p, float x) ;
+
+void detruire_polynome (p_polyf_t p) ;
+
+p_polyf_t lire_polynome_float (char *nom_fichier) ;
+
+void ecrire_polynome_float (p_polyf_t p) ;
+
+int egalite_polynome (p_polyf_t p1, p_polyf_t p2) ;
+
+p_polyf_t addition_polynome (p_polyf_t p1, p_polyf_t p2) ;
+
+p_polyf_t multiplication_polynome_scalaire (p_polyf_t p, float alpha) ;
+
+float eval_polynome (p_polyf_t p, float x) ;
+
+p_polyf_t multiplication_polynomes (p_polyf_t p1, p_polyf_t p2) ;
+
+p_polyf_t puissance_polynome (p_polyf_t p, int n) ;
+
+p_polyf_t composition_polynome (p_polyf_t p, p_polyf_t q) ;
