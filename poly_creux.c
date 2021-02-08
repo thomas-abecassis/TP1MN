fb74fce67c957e5da2f7c781cae7da31f77e23aa
lucas tyndal <lucas.tyndal@etu.univ-grenoble-alpes.fr>
2021-02-08 11:05:46 +0100

    a

--- poly_creux.c
+++ poly_creux.c
@@ -32,7 +32,8 @@ p_polyf_creux_t creer_polynome_tab (int nbDegre, int* degres, float* coeffs){
 
 void detruire_polynome (p_polyf_creux_t p)
 {
-  
+  free(p->element);
+  free(p);
 
   return ;
 }
@@ -52,7 +53,10 @@ void init_polynome (p_polyf_creux_t p, float x)
 
 void ecrire_polynome_float (p_polyf_creux_t p)
 {
-  
+  printf("%f X^%d",p->element[0].coeff,p-element[0].degre);
+  for (int i=1;i<=p->nb_degre,i++){
+    printf("+ %f * X^%d",p->element[i].coeff,p-element[i].degre);
+  }
 
   return ;
 }
@@ -66,18 +70,67 @@ int egalite_polynome (p_polyf_creux_t p1, p_polyf_creux_t p2)
       return 0;
   }
   return 1;
+
 }
 
 p_polyf_creux_t addition_polynome (p_polyf_creux_t p1, p_polyf_creux_t p2)
 {
-  return NULL;
+  int nb_degre=0;
+  int i1=0;
+  int i2=0;
+  while(i1!=p1->nb_degre && i2!=p2->nb_degre){
+    if(p1->element[i1].degre==p1->element[i2].degre){
+      nb_degre++;
+      i1++;
+      i2++;
+    }
+    if(p1->element[i1].degre<p1->element[i2].degre){
+      nb_degre++;
+      i2++;
+    }
+    else{
+      nb_degre++;
+      i1++;
+    }
+  }
+  p_polyf_creux_t p=creer_polynome(nb_degre);
+  i1=0;
+  i2=0;
+  i=0;
+  while(i1!=p1->nb_degre && i2!=p2->nb_degre){
+    if(p1->element[i1].degre==p1->element[i2].degre){
+      p->element[i].coeff=p1->element[i1].coeff+p2->element[i2].coeff;
+      p->element[i].degre=p1->element[i1]->degre;
+      i1++;
+      i++;
+      i2++;
+    }
+    if(p1->element[i1].degre<p1->element[i2].degre){
+      p->element[i].coeff=p2->element[i2].coeff;
+      p->element[i].degre=p2->element[i2].degre;
+      i++;
+      i2++;
+    }
+    else{
+      p->element[i].coeff=p1->element[i1].coeff;
+      p->element[i].degre=p1->element[i1].degre;
+      i++
+      i1++;
+    }
+  }
+  return p;
 }
 
 p_polyf_creux_t multiplication_polynome_scalaire (p_polyf_creux_t p, float alpha)
 {
+  p_polyf_creux_t new_p=creer_polynome(p->nb_degre);
+  for(int i=0;i<=p->degre;i++){
+    new_p->element[i].coeff=p->element[i].coeff*alpha;
+    new_p->element[i].degre=p->element[i].degre;
+  }
   
 
-  return NULL ;
+  return new_p ;
 }
 
 float eval_polynome (p_polyf_creux_t p, float x)
